22765d21f25a092033d20789e6a9d98ea81484b5
Josh Taylor <josh@joshtaylordesign.com>
2021-06-27 17:46:27 -0700

    update

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import 'normalize.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "./components/Home.js"
+import Work from "./components/Work.js"
+import About from "./components/About.js"
+import Writing from "./components/Writing.js"
+import Head from "./components/Head.js"
+import WorkDetail from "./components/WorkDetail.js"
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/work">
+              <Head />
+              <Work />
+            </Route>
+            <Route exact path="/about">
+              <Head />
+              <About />
+            </Route>
+            <Route exact path="/writing">
+              <Head />
+              <Writing />
+            </Route>
+            <Route exact path="/work/:id">
+              <Head />
+              <WorkDetail />
+            </Route>
+            <Route exact path="/">
+              <Head />
+              <Home />
+            </Route>
+            <Route exact path="*">
+              <Head />
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ client/src/assets/JT_Logo.js
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+
+const Logo = () => (
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180.5 176">
+      <path d="M57.2 164l23.4-7.6c3.4-1 5.7-4.2 5.7-7.7V51.2H57.2V164z"/>
+      <path d="M162.9 21.7c0-4.8-3.9-8.7-8.7-8.7h-28.5H96.6v29.1 98.6h29.1V42.1h37.2V21.7z"/>
+      <path d="M57.2 21.7v20.4h21c4.5 0 8.1-3.6 8.1-8.1V13H65.9C61.1 13 57.2 16.9 57.2 21.7z"/>
+</svg>
+);
+
+export default Logo;
