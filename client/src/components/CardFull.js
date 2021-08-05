f0d75a9fd57a0bb345fed336808a1324a0569410
Josh Taylor <josh@joshtaylordesign.com>
2021-08-05 00:10:06 -0700

    club hal detail

--- client/src/components/CardFull.js
+++ client/src/components/CardFull.js
@@ -7,13 +7,13 @@ export default function CardFull(props) {
     const history = useHistory();
     
     const handleClick = (e) => {
-        console.log('clicked');
         history.push(`/work/`+ props.title);
     }
     return (
         <div className="card" onClick={handleClick}>
             <div className="label" id={props.id}>
                 <h1>{props.title}</h1>
+                <p>click for more details</p>
             </div>
             <img id='contain' src={props.image}/>
         </div>
