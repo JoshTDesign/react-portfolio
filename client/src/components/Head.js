3c3459e99c14ced178d52ce826846f9a1f9eb9a1
Josh Taylor <josh@joshtaylordesign.com>
2021-08-19 08:45:43 -0700

    tab work

--- client/src/components/Head.js
+++ client/src/components/Head.js
@@ -1,15 +1,39 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { Link, useHistory } from 'react-router-dom'
 import Logo from '../assets/JT_Logo.js'
 import Logosvg from '../assets/JT_Logo.svg'
-import Tab from './Tab.js'
+// import Tab from './Tab.js'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import 'react-tabs/style/react-tabs.css';
 
 export default function Head() {
-    
-const tabActive = () => {
 
+    const history = useHistory();
+
+    const [tabIndex, setTabIndex] = useState(null);
+
+
+const testFunction = (n) => {
+    // console.log(n);
+    switch(n) {
+        case 0:
+          history.push(`/work`)
+          break;
+        case 1:
+            history.push(`/projects`)
+            break;
+        case 2:
+            history.push(`/about`)
+            break;
+        case 3:
+            history.push(`/resume`)
+            break;
+        default:
+          console.log('default');
+      }
+      setTabIndex(n);
 }
-    
-    
     return (
         // <div id="container">
             <div id="head">
@@ -18,13 +42,14 @@ const tabActive = () => {
                 </a>
                 <p>Josh Taylor is a designer and developer based in Seattle.</p>
                 
-                <ul style={{padding:0}}>
-                    {/* <li><a href="/work">Design</a></li> */}
-                    <Tab text="Design" status="test"/>
-                    <li><a href="/projects">Web Apps</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/resume">Resume</a></li>
-                </ul>
+                <Tabs selectedIndex={tabIndex} onSelect={testFunction}>
+                    <TabList>
+                    <Tab>Design</Tab>
+                    <Tab>Web Apps</Tab>
+                    <Tab>About</Tab>
+                    <Tab>Resum&#xC9;</Tab>
+                    </TabList>
+                </Tabs>
             </div>
         // </div>
     )
