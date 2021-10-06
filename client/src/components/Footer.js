66ee473e409d10b5b4948df8f1c73600886f3c10
Josh Taylor <josh@joshtaylordesign.com>
2021-10-05 19:16:53 -0700

    animation

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,21 @@ import Logosvg from '../assets/JT_Logo.svg'
 
 export default function Footer() {
     
-    
-    
+    function draw() {
+        var canvas = document.getElementById('canvas');
+        if (canvas.getContext) {
+          var ctx = canvas.getContext('2d');
+  
+          ctx.fillStyle = 'rgb(30,30,30)';
+          ctx.fillRect(10, 10, 50, 50);
+  
+          ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+          ctx.fillRect(30, 30, 50, 50);
+        }
+      }
+
+
+
     return (
             <div id="footer">
                 <p>designed and coded by Josh Taylor</p>
