ada36c58ffec55751111c548ae96d39ffcb9a3ac
Josh Taylor <josh@joshtaylordesign.com>
2021-11-17 11:29:58 -0800

    about update

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,7 +5,19 @@ export default function Home() {
     return (
         <div id="contact">
             <img src={Headshot2} style={{width:'275px'}}/>
-            <p>I am a Seattle-based designer with years of experience building strong brands in variety of media. Currently, I am focusing on digital experiences and I am available for work.  If you have any questions, I would love to hear from you.</p>
+            <div id='bio'>
+                <ul id='tech'>
+                    <li>Branding</li>
+                    <li>UX/UI Design</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>ReactJS</li>
+                    <li>Node.js</li>
+                    <li>MySQL</li>
+                </ul>
+                <p id='intro'>I am a Seattle-based designer with years of experience building strong brands in variety of media. Currently, I am focusing on digital experiences and I am available for work.  If you have any questions, I would love to hear from you.</p>
+            </div>
         </div>
     )
 }
